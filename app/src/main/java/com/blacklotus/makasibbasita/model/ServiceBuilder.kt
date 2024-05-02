167d5e69421101d75e6b8466461c7af6441c08c0
BadRooBot <87873962+BadRooBot@users.noreply.github.com>
2024-05-02 20:14:07 +0300

    Update ServiceBuilder.kt

--- app/src/main/java/com/blacklotus/makasibbasita/model/ServiceBuilder.kt
+++ app/src/main/java/com/blacklotus/makasibbasita/model/ServiceBuilder.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceBuilder {
-    private const val BASE_URL = "https://makasib-basita-server.onrender.com"
+    private const val BASE_URL = "back end url"
     private  val okHttp: OkHttpClient.Builder = OkHttpClient.Builder()
     private val builder: Retrofit.Builder = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
         .client(okHttp.build())
